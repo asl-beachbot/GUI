0bec39498cdb879b0602f9a9828717a6d569aeea
Markus Stäuble <markusta@student.ethz.ch>
2014-03-12 15:05:21 +0100

    update

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from werkzeug.routing import NumberConverter, ValidationError
 import json
 app = Flask(__name__)
+
+ 
+class NegativeFloatConverter(NumberConverter):
+    regex = r'\-?\d+\.\d+'
+    num_convert = float
+     
+    def __init__(self, map, min=None, max=None):
+        NumberConverter.__init__(self, map, 0, min, max)
+ 
+app.url_map.converters['float'] = NegativeFloatConverter
 
 @app.route("/")
 def hello():
